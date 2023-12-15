e63ab06a2c69efd5199f2be5000f88ea625b1342
Konrad <konrad.wojtylo.9@gmail.com>
2023-12-15 17:39:56 +0100

    Created main section and added header, footer components for future work

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,55 @@
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+
+import { Link } from 'react-router-dom';
+
 export const App: React.FC = () => {
 	return (
-		<main>
-			<div className='wrapper'></div>
-		</main>
+		<>
+			<Header />
+			<main>
+				<div className='wrapper'>
+					<div className='main-section'>
+						<div className='main-section__first-frame'>
+							<h3 className='main-section__title'>
+								duże <br /> obrazy
+							</h3>
+							<Link to='/' className='main-section__button'>
+								zobacz
+							</Link>
+						</div>
+						<div className='main-section__second-frame'>
+							<h3 className='main-section__title'>
+								średnie <br /> obrazy
+							</h3>
+							<Link to='/' className='main-section__button'>
+								zobacz
+							</Link>
+						</div>
+						<div className='main-section__third-frame'>
+							<h3 className='main-section__title'>
+								małe <br /> obrazy
+							</h3>
+							<Link to='/' className='main-section__button'>
+								zobacz
+							</Link>
+						</div>
+						<div className='main-section__fourth-frame'>
+							<h3 className='main-section__title'>kontakt</h3>
+							<Link to='/' className='main-section__button'>
+								napisz
+							</Link>
+						</div>
+						<div className='main-section__fifth-frame'>
+							<h3 className='main-section__title'>regulamin</h3>
+							<Link to='/' className='main-section__button'>
+								zobacz
+							</Link>
+						</div>
+					</div>
+				</div>
+			</main>
+			<Footer />
+		</>
 	);
 };
