96708b6d7b2331eb9f628971167da57c12c52afc
Konrad <konrad.wojtylo.9@gmail.com>
2023-12-24 19:46:56 +0100

    Redesigned main-section

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,12 @@
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 
-import { Link } from 'react-router-dom';
-
 export const App: React.FC = () => {
 	return (
 		<>
 			<Header />
 			<main>
-				<div className='wrapper'>
-					<div className='main-section'>
-						<div className='main-section__first-frame'>
-							<h3 className='main-section__title'>
-								duże <br /> obrazy
-							</h3>
-							<Link to='/' className='main-section__button'>
-								zobacz
-							</Link>
-						</div>
-						<div className='main-section__second-frame'>
-							<h3 className='main-section__title'>
-								średnie <br /> obrazy
-							</h3>
-							<Link to='/' className='main-section__button'>
-								zobacz
-							</Link>
-						</div>
-						<div className='main-section__third-frame'>
-							<h3 className='main-section__title'>
-								małe <br /> obrazy
-							</h3>
-							<Link to='/' className='main-section__button'>
-								zobacz
-							</Link>
-						</div>
-					</div>
-				</div>
+				<div className='main-section'></div>
 			</main>
 			<Footer />
 		</>
