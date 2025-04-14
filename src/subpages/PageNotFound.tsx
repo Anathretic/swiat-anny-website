95806a385c566713fab036dc695bae6caa6600c7
Konrad <konrad.wojtylo.9@gmail.com>
2025-04-14 14:46:12 +0200

    Updated structure syntax in subpages

--- src/subpages/PageNotFound.tsx
+++ src/subpages/PageNotFound.tsx
@@ -3,15 +3,17 @@ import NotFoundImage from '../images/notfound-img.svg';
 
 const PageNotFound: React.FC = () => {
 	return (
-		<section className='notfound'>
-			<img className='notfound__img' src={NotFoundImage} alt='Page not found image' />
-			<a href='https://storyset.com/online' className='notfound__credits' target='_blank'>
-				Error 404 image by Storyset
-			</a>
-			<Link to='/' className='notfound__btn'>
-				home
-			</Link>
-		</section>
+		<main>
+			<section className='notfound'>
+				<img className='notfound__img' src={NotFoundImage} alt='Page not found image' />
+				<a href='https://storyset.com/online' className='notfound__credits' target='_blank'>
+					Error 404 image by Storyset
+				</a>
+				<Link to='/' className='notfound__btn'>
+					home
+				</Link>
+			</section>
+		</main>
 	);
 };
 
