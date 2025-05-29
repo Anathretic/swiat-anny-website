5f57b5ad22912ef0770fc1d5d0887a0cae892167
Konrad <konrad.wojtylo.9@gmail.com>
2025-05-29 21:52:20 +0200

    Added features and fixes to the welcome section

--- src/sections/Welcome.tsx
+++ src/sections/Welcome.tsx
@@ -10,12 +10,13 @@ const Welcome: React.FC = () => {
 					<span>i</span>
 					<span>a</span>
 					<span>t</span>
-					<br className='welcome__special-br-tag'/>
+					<br className='welcome__special-br-tag' />
 					<span>A</span>
 					<span>n</span>
 					<span>n</span>
 					<span>y</span>
 				</h1>
+				<p className='welcome__subtext'>Kolory, które poruszają. Obrazy, które mówią bez słów.</p>
 				<HashLink smooth to='/#oferta' className='welcome__button'>
 					odkryj
 				</HashLink>
